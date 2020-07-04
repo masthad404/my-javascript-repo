90a5bcb97df004fb1caa4a2058de993369cb43a4
masthad404 <67835158+masthad404@users.noreply.github.com>
2020-07-04 18:01:10 +0200

    Create first-else.if.js
    My first else,else if .

---
+++ first-else.if.js
@@ -0,0 +1,20 @@
+var ageOFGyula = 13;
+
+if (ageOFGyula < 16){
+   
+    console.log('gyula nem vezethet');
+
+}
+
+else if (ageOFGyula == 16){
+
+    console.log('Gyula már majdnem vezethet!');
+
+}
+
+else if (ageOFGyula >= 17){
+
+    console.log('Gyula már vezethet!');
+
+}
+
