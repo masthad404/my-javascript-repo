2b7098ce0401df13fadacb6a1c317c49ddc6322e
masthad404 <67835158+masthad404@users.noreply.github.com>
2020-07-04 17:56:39 +0200

    Create first-array.js
    My first array.

---
+++ first-array.js
@@ -0,0 +1,9 @@
+var fruits = ['banana', 'grape', 'apple', 'watermelone', 'lime' ];
+
+var size = fruits.length;
+
+for(i = 0	; i < fruits.length; i++){
+
+	  alert('A fruits tömb jelenlegi értéke ' + fruits[i] + '. Sorszáma: ' + i);
+
+}
